73c972b4885f4682937c33ee0b77caeec59f5eb9
mesta <meesta@hotmail.it>
2016-06-16 14:11:55 +0200

    sad

--- test.js
+++ test.js
@@ -1,5 +1,19 @@
-
- 
  $(document).ready(function(){
-	  window.HtmlViewer.showHTML( "ready");
+	var songs = $(".item");
+	var array_songs = new Array();
+	for(var i=0;songs.length;i++){
+		var song = songs[i];
+		var song_name = "";
+		var artist_name = "";
+		var album_name = "";
+		
+		var obj_song = new Object();
+		obj_song.song_name = song_name;
+		obj_song.artist_name = artist_name;
+		obj_song.album_name = album_name;
+		array_songs.push(obj_song);
+	}
+	var obj_temp = new Object();
+	obj_temp.songs = array_songs;
+	window.HtmlViewer.showHTML(JSON.stringify(obj_temp));
  });
